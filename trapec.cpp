a22103eab62f2c995b1d35946bd27532014d07dd
mrgatre109-sudo <miskova007g@gmail.com>
2026-03-26 17:55:16 +0700

    fix

--- trapec.cpp
+++ trapec.cpp
@@ -14,14 +14,27 @@ int main() {
       double side1, side2, side3, side4;
       cout << "Введите длины четырёх сторон трапеции:" << endl;
       cin >> side1 >> side2 >> side3 >> side4;
+      if (side1 < 0 || side2 < 0 || side3 < 0 || side4 < 0){
+                cout << "side can't < 0" << endl;
+                return 0; 
+            }
+      if ((side1 + side2 + side3) <= side4 || (side1 + side2 + side4) <= side3 || (side1 + side3 + side4) <= side2 || (side2 + side3 + side4) <= side1){
+      cout << "Neverno, tak nelza" << endl; 
+        return 1;
+      } 
       double perimetr = side1 + side2 + side3 + side4;
       cout << perimetr << endl;
       break;
+    
     }
     case 2: {
       double hieght, base1, base2;
       cout << "Введите высоту и основания трапеции:" << endl;
       cin >> hieght >> base1 >> base2;
+      if (hieght < 0 || base1 < 0 || base2 < 0){
+                cout << "side can't < 0" << endl;
+                return 0; 
+            }
       double area = (hieght * (base1 + base2)) / 2;
       cout << area << endl;
       break;
@@ -30,6 +43,10 @@ int main() {
       double base1, base2;
       cout << "Введите основания трапеции:" << endl;
       cin >> base1 >> base2;
+      if (base1 < 0 || base2 < 0){
+                cout << "side can't < 0" << endl;
+                return 0; 
+            }
       double middle = (base1 + base2) / 2;
       cout << middle << endl;
       break;
